cf0121eb66ececf381c61941a64a58673b40ba23
ramesh <gouru.ramesh@gmail.com>
2023-06-22 18:03:15 +0800

    application

--- src/main/java/com/guru/shippment/ShipmentApplication.java
+++ src/main/java/com/guru/shippment/ShipmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShippmentApplication {
+public class ShipmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShippmentApplication.class, args);
+		SpringApplication.run(ShipmentApplication.class, args);
 	}
 
 }
